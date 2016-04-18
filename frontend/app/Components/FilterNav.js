1c4d0fd2a1d283c6575bb90ac3898dff89da94b0
MichaelDM <michaelmachiah@gmail.com>
2016-04-17 22:11:20 -0400

    eureka! filter ok :)

--- frontend/app/Components/FilterNav.js
+++ frontend/app/Components/FilterNav.js
@@ -3,23 +3,29 @@ import FilterNavUI from './FilterNavUI';
 
 const FilterNav = React.createClass({
   handleFilter(e){
-    var classTarget = e.target.innerText;
-    console.log(classTarget);
-    var elTargets = document.querySelectorAll('.'+classTarget);
-    console.log('elements are ', elTargets);
+    // var classTarget = e.target.innerText;
+    // console.log(classTarget);
+    // var elTargets = document.querySelectorAll('.'+classTarget);
+    // console.log('elements are ', elTargets);
     var allGrids = document.querySelectorAll('.gridImage');
     console.log(allGrids);
 
     // loop through DOM elements
-    for (var i=0; i<elTargets.length; i++){
+    for (var i=0; i<allGrids.length; i++){
       // if DOM element has class
-      if (elTargets[i].classList.length){
+      if (allGrids[i].classList.length){
         // loop through array of class
-        for (var j=0; j<elTargets[i].classList.length; j++){
+        let classResult = false;
+        for (var j=0; j<allGrids[i].classList.length; j++){
           // if one element in array matches or filter
-          if (elTargets[i].classList[j] === e.target.innerText){
-            console.log('found');
+          if (allGrids[i].classList[j] === e.target.innerText){
+            classResult= true;
           }
+        }
+        if (classResult){
+          allGrids[i].style.display = 'inline-block';
+        } else {
+          allGrids[i].style.display = 'none';
         }
       }
     }
