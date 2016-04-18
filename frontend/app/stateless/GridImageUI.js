ec1a3228f79974885761ed64db9f3903121f94af
MichaelDM <michaelmachiah@gmail.com>
2016-04-18 00:34:39 -0400

    all Skills but not as a list yet

--- frontend/app/stateless/GridImageUI.js
+++ frontend/app/stateless/GridImageUI.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
+const SkillList= props => {
+  return (
+    <li>{props.allSkills}</li>
+  )
+};
+
 const GridImageUI = React.createClass({
   render(){
     const returnClass = function(skills) {
@@ -9,7 +15,17 @@ const GridImageUI = React.createClass({
     };
     return (
       <figure className={returnClass(this.props.object.skills)+' gridImage'}>
-        <img src={this.props.object.thumbnail} />
+        <span className="imageWrapper">
+          <img src={this.props.object.thumbnail} />
+        </span>
+        <span className="skillListGrid">
+          <ul>
+            <SkillList allSkills={this.props.object.skills}/>
+          </ul>
+        </span>
+        <div className="projTitle">
+          {this.props.object.title}
+        </div>
       </figure>
     )
   }
